8a624e6f117eed09177ae381e3b860ecc9c5b97d
Push911 <49253201+Push911@users.noreply.github.com>
2020-08-05 04:00:17 +0200

    Rewrited pneumoniadetect.py

--- pneumonia.py
+++ pneumonia.py
@@ -1,18 +1,18 @@
 import sys
 from os.path import expanduser
-
 from PyQt5 import QtCore
-from PyQt5.QtCore import QSize, QTimer, QRunnable, QThreadPool, pyqtSlot
+from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QMovie, QPixmap, QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QLabel, QWidget, QFileDialog, QTableWidget, QTableWidgetItem, QVBoxLayout, QAbstractItemView
-from pneumoniadetect import global_test , modelsDir, global_predict
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QLabel, QFileDialog, QTableWidget, \
+    QTableWidgetItem, QAbstractItemView
+from pneumoniadetect import modelsTest, modelsDir, modelsPrediction
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setGeometry(540, 250, 600, 750)
-        self.setWindowTitle("PneumoniaDetectorPro")
+        self.setWindowTitle("Pneumonia Detector")
         self.setWindowIcon(QIcon("corona.ico"))
         self.setFixedSize(900, 600)
         self.setStyleSheet("background-color: #3a6186")
@@ -68,158 +68,158 @@ class MainWindow(QMainWindow):
 
         self.andrei_name = QLabel(self)
         self.andrei_name.setText("Andrei Shpakouski")
-        self.andrei_name.setFont(QFont('Courier', 20, weight=QFont.Bold))
+        self.andrei_name.setFont(QFont("Courier", 20, weight=QFont.Bold))
         self.andrei_name.setGeometry(215, 20, 300, 30)
-        self.andrei_name.setStyleSheet('background-color: #3a6186')
+        self.andrei_name.setStyleSheet("background-color: #3a6186")
         self.andrei_name.hide()
 
         self.andrei_role = QLabel(self)
-        self.andrei_role.setText('Role:')
-        self.andrei_role.setFont(QFont('Courier', 15, weight=QFont.Bold))
+        self.andrei_role.setText("Role:")
+        self.andrei_role.setFont(QFont("Courier", 15, weight=QFont.Bold))
         self.andrei_role.setGeometry(215, 60, 55, 20)
-        self.andrei_role.setStyleSheet('background-color: #3a6186')
+        self.andrei_role.setStyleSheet("background-color: #3a6186")
         self.andrei_role.hide()
 
         self.andrei_role_fact = QLabel(self)
-        self.andrei_role_fact.setText('Backend & Frontend')
-        self.andrei_role_fact.setFont(QFont('Courier', 12))
+        self.andrei_role_fact.setText("Backend & Frontend")
+        self.andrei_role_fact.setFont(QFont("Courier", 12))
         self.andrei_role_fact.setGeometry(280, 62, 250, 20)
-        self.andrei_role_fact.setStyleSheet('background-color: #3a6186')
+        self.andrei_role_fact.setStyleSheet("background-color: #3a6186")
         self.andrei_role_fact.hide()
 
         self.andrei_imp = QLabel(self)
-        self.andrei_imp.setText('Introduces:')
-        self.andrei_imp.setFont(QFont('Courier', 14, weight=QFont.Bold))
+        self.andrei_imp.setText("Introduces:")
+        self.andrei_imp.setFont(QFont("Courier", 14, weight=QFont.Bold))
         self.andrei_imp.setGeometry(215, 90, 122, 20)
-        self.andrei_imp.setStyleSheet('background-color: #3a6186')
+        self.andrei_imp.setStyleSheet("background-color: #3a6186")
         self.andrei_imp.hide()
 
         self.andrei_imp_facts1 = QLabel(self)
-        self.andrei_imp_facts1.setText('- Created global_test and global_predict functions')
+        self.andrei_imp_facts1.setText("- Created modelsTest and modelsPrediction functions")
         self.andrei_imp_facts1.setGeometry(342, 93, 497, 15)
-        self.andrei_imp_facts1.setFont(QFont('Courier', 12))
-        self.andrei_imp_facts1.setStyleSheet('background-color: #3a6186;')
+        self.andrei_imp_facts1.setFont(QFont("Courier", 12))
+        self.andrei_imp_facts1.setStyleSheet("background-color: #3a6186;")
         self.andrei_imp_facts1.hide()
 
         self.andrei_imp_facts2 = QLabel(self)
-        self.andrei_imp_facts2.setText('- Configured test and predict functions for GUI needs')
+        self.andrei_imp_facts2.setText("- Configured test and predict functions for GUI needs")
         self.andrei_imp_facts2.setGeometry(342, 110, 510, 15)
-        self.andrei_imp_facts2.setFont(QFont('Courier', 12))
-        self.andrei_imp_facts2.setStyleSheet('background-color: #3a6186;')
+        self.andrei_imp_facts2.setFont(QFont("Courier", 12))
+        self.andrei_imp_facts2.setStyleSheet("background-color: #3a6186;")
         self.andrei_imp_facts2.hide()
 
         self.andrei_imp_facts3 = QLabel(self)
         self.andrei_imp_facts3.setWordWrap(True)
-        self.andrei_imp_facts3.setText('- Added and taught MobileNetV2 and VGG16 models')
+        self.andrei_imp_facts3.setText("- Added and taught MobileNetV2 and VGG16 models")
         self.andrei_imp_facts3.setGeometry(342, 127, 550, 15)
-        self.andrei_imp_facts3.setFont(QFont('Courier', 12))
-        self.andrei_imp_facts3.setStyleSheet('background-color: #3a6186;')
+        self.andrei_imp_facts3.setFont(QFont("Courier", 12))
+        self.andrei_imp_facts3.setStyleSheet("background-color: #3a6186;")
         self.andrei_imp_facts3.hide()
 
         self.vova_name = QLabel(self)
         self.vova_name.setText("Volodymyr Mamedov")
-        self.vova_name.setFont(QFont('Courier', 20, weight=QFont.Bold))
+        self.vova_name.setFont(QFont("Courier", 20, weight=QFont.Bold))
         self.vova_name.setGeometry(215, 167, 300, 30)
-        self.vova_name.setStyleSheet('background-color: #3a6186')
+        self.vova_name.setStyleSheet("background-color: #3a6186")
         self.vova_name.hide()
 
         self.vova_role = QLabel(self)
-        self.vova_role.setText('Role:')
-        self.vova_role.setFont(QFont('Courier', 15, weight=QFont.Bold))
+        self.vova_role.setText("Role:")
+        self.vova_role.setFont(QFont("Courier", 15, weight=QFont.Bold))
         self.vova_role.setGeometry(215, 207, 55, 20)
-        self.vova_role.setStyleSheet('background-color: #3a6186')
+        self.vova_role.setStyleSheet("background-color: #3a6186")
         self.vova_role.hide()
 
         self.vova_role_fact = QLabel(self)
-        self.vova_role_fact.setText('Backend')
-        self.vova_role_fact.setFont(QFont('Courier', 12))
+        self.vova_role_fact.setText("Backend")
+        self.vova_role_fact.setFont(QFont("Courier", 12))
         self.vova_role_fact.setGeometry(280, 209, 250, 20)
-        self.vova_role_fact.setStyleSheet('background-color: #3a6186')
+        self.vova_role_fact.setStyleSheet("background-color: #3a6186")
         self.vova_role_fact.hide()
 
         self.vova_imp = QLabel(self)
-        self.vova_imp.setText('Introduces:')
-        self.vova_imp.setFont(QFont('Courier', 14, weight=QFont.Bold))
+        self.vova_imp.setText("Introduces:")
+        self.vova_imp.setFont(QFont("Courier", 14, weight=QFont.Bold))
         self.vova_imp.setGeometry(215, 237, 122, 20)
-        self.vova_imp.setStyleSheet('background-color: #3a6186')
+        self.vova_imp.setStyleSheet("background-color: #3a6186")
         self.vova_imp.hide()
 
         self.vova_imp_facts1 = QLabel(self)
-        self.vova_imp_facts1.setText('- Added and taught DenseNet201 and InceptionV3 models')
+        self.vova_imp_facts1.setText("- Added and taught DenseNet201 and InceptionV3 models")
         self.vova_imp_facts1.setGeometry(342, 240, 497, 15)
-        self.vova_imp_facts1.setFont(QFont('Courier', 12))
-        self.vova_imp_facts1.setStyleSheet('background-color: #3a6186;')
+        self.vova_imp_facts1.setFont(QFont("Courier", 12))
+        self.vova_imp_facts1.setStyleSheet("background-color: #3a6186;")
         self.vova_imp_facts1.hide()
 
         self.vova_imp_facts2 = QLabel(self)
-        self.vova_imp_facts2.setText('- Help to create global_predict and global_test functions')
+        self.vova_imp_facts2.setText("- Help to create modelsPrediction and modelsTest functions")
         self.vova_imp_facts2.setGeometry(342, 257, 550, 15)
-        self.vova_imp_facts2.setFont(QFont('Courier', 12))
-        self.vova_imp_facts2.setStyleSheet('background-color: #3a6186;')
+        self.vova_imp_facts2.setFont(QFont("Courier", 12))
+        self.vova_imp_facts2.setStyleSheet("background-color: #3a6186;")
         self.vova_imp_facts2.hide()
 
         self.vova_imp_facts3 = QLabel(self)
         self.vova_imp_facts3.setWordWrap(True)
-        self.vova_imp_facts3.setText('- Optimized functions work for the processor')
+        self.vova_imp_facts3.setText("- Optimized functions work for the processor")
         self.vova_imp_facts3.setGeometry(342, 274, 550, 15)
-        self.vova_imp_facts3.setFont(QFont('Courier', 12))
-        self.vova_imp_facts3.setStyleSheet('background-color: #3a6186;')
+        self.vova_imp_facts3.setFont(QFont("Courier", 12))
+        self.vova_imp_facts3.setStyleSheet("background-color: #3a6186;")
         self.vova_imp_facts3.hide()
 
         self.valera_name = QLabel(self)
         self.valera_name.setText("Valerii Bahrov")
-        self.valera_name.setFont(QFont('Courier', 20, weight=QFont.Bold))
+        self.valera_name.setFont(QFont("Courier", 20, weight=QFont.Bold))
         self.valera_name.setGeometry(215, 314, 300, 30)
-        self.valera_name.setStyleSheet('background-color: #3a6186')
+        self.valera_name.setStyleSheet("background-color: #3a6186")
         self.valera_name.hide()
 
         self.valera_role = QLabel(self)
-        self.valera_role.setText('Role:')
+        self.valera_role.setText("Role:")
         self.valera_role.setFont(QFont('Courier', 15, weight=QFont.Bold))
         self.valera_role.setGeometry(215, 354, 55, 20)
-        self.valera_role.setStyleSheet('background-color: #3a6186')
+        self.valera_role.setStyleSheet("background-color: #3a6186")
         self.valera_role.hide()
 
         self.valera_role_fact = QLabel(self)
-        self.valera_role_fact.setText('Frontend')
-        self.valera_role_fact.setFont(QFont('Courier', 12))
+        self.valera_role_fact.setText("Frontend")
+        self.valera_role_fact.setFont(QFont("Courier", 12))
         self.valera_role_fact.setGeometry(280, 356, 250, 20)
-        self.valera_role_fact.setStyleSheet('background-color: #3a6186')
+        self.valera_role_fact.setStyleSheet("background-color: #3a6186")
         self.valera_role_fact.hide()
 
         self.valera_imp = QLabel(self)
-        self.valera_imp.setText('Introduces:')
-        self.valera_imp.setFont(QFont('Courier', 14, weight=QFont.Bold))
+        self.valera_imp.setText("Introduces:")
+        self.valera_imp.setFont(QFont("Courier", 14, weight=QFont.Bold))
         self.valera_imp.setGeometry(215, 384, 122, 20)
-        self.valera_imp.setStyleSheet('background-color: #3a6186')
+        self.valera_imp.setStyleSheet("background-color: #3a6186")
         self.valera_imp.hide()
 
         self.valera_imp_facts1 = QLabel(self)
-        self.valera_imp_facts1.setText('- Created most of the GUI')
+        self.valera_imp_facts1.setText("- Created most of the GUI")
         self.valera_imp_facts1.setGeometry(342, 387, 497, 15)
         self.valera_imp_facts1.setFont(QFont('Courier', 12))
-        self.valera_imp_facts1.setStyleSheet('background-color: #3a6186;')
+        self.valera_imp_facts1.setStyleSheet("background-color: #3a6186;")
         self.valera_imp_facts1.hide()
 
         self.valera_imp_facts2 = QLabel(self)
-        self.valera_imp_facts2.setText('- Help to configure predict and test function for GUI needs')
+        self.valera_imp_facts2.setText("- Help to configure predict and test function for GUI needs")
         self.valera_imp_facts2.setGeometry(342, 404, 510, 15)
-        self.valera_imp_facts2.setFont(QFont('Courier', 12))
-        self.valera_imp_facts2.setStyleSheet('background-color: #3a6186;')
+        self.valera_imp_facts2.setFont(QFont("Courier", 12))
+        self.valera_imp_facts2.setStyleSheet("background-color: #3a6186;")
         self.valera_imp_facts2.hide()
 
         self.valera_imp_facts3 = QLabel(self)
         self.valera_imp_facts3.setWordWrap(True)
-        self.valera_imp_facts3.setText('- Itroduced predict and test function to GUI')
+        self.valera_imp_facts3.setText("- Introduced predict and test function to GUI")
         self.valera_imp_facts3.setGeometry(342, 421, 550, 15)
-        self.valera_imp_facts3.setFont(QFont('Courier', 12))
-        self.valera_imp_facts3.setStyleSheet('background-color: #3a6186;')
+        self.valera_imp_facts3.setFont(QFont("Courier", 12))
+        self.valera_imp_facts3.setStyleSheet("background-color: #3a6186;")
         self.valera_imp_facts3.hide()
 
         self.button_refresh = QPushButton(self)
         self.button_refresh.setGeometry(10, 540, 185, 50)
-        self.button_refresh.setIcon(QIcon('refresh.png'))
-        self.button_refresh.setFont(QFont('Roboto', 15))
+        self.button_refresh.setIcon(QIcon("refresh.png"))
+        self.button_refresh.setFont(QFont("Roboto", 15))
         self.button_refresh.setIconSize(QSize(25, 25))
         self.button_refresh.setStyleSheet("background-color: #254059;")
         self.button_refresh.setText("Refresh")
@@ -235,22 +235,22 @@ class MainWindow(QMainWindow):
                                     'young children, people older than age 65, and people with health problems or '
                                     'weakened immune systems.')
         self.pneumonia_info.setGeometry(10, 90, 190, 310)
-        self.pneumonia_info.setFont(QFont('Roboto', 10))
-        self.pneumonia_info.setStyleSheet('background-color: #254059;')
+        self.pneumonia_info.setFont(QFont("Roboto", 10))
+        self.pneumonia_info.setStyleSheet("background-color: #254059;")
         self.pneumonia_info.setAlignment(QtCore.Qt.AlignCenter)
 
         self.greetings = QLabel(self)
         self.greetings.setWordWrap(True)
         self.greetings.setText(
-            'Welcome to the most "accurate" pneumonia detector ever! \n Choose an option and enjoy our app!')
+            'Welcome to the pneumonia detector! \n Choose an option and enjoy our app!')
         self.greetings.setGeometry(210, 30, 700, 100)
-        self.greetings.setFont(QFont('Courier', 15, weight=QFont.Bold))
-        self.greetings.setStyleSheet('background-color: #3a6186')
+        self.greetings.setFont(QFont("Courier", 15, weight=QFont.Bold))
+        self.greetings.setStyleSheet("background-color: #3a6186")
         self.greetings.setAlignment(QtCore.Qt.AlignCenter)
 
         self.virus_label = QLabel(self)
         self.movie = QMovie(self)
-        self.movie.setFileName('neponel.gif')
+        self.movie.setFileName("neponel.gif")
         self.movie.setScaledSize(QSize(400, 400))
         self.virus_label.setGeometry(355, 130, 400, 400)
         self.virus_label.setMovie(self.movie)
@@ -258,18 +258,18 @@ class MainWindow(QMainWindow):
 
         self.loading_label = QLabel(self)
         self.loading_label.setGeometry(450, 110, 400, 400)
-        self.loading_label.setText('Loading...')
-        self.loading_label.setFont(QFont('Courier', 30, weight=QFont.Bold))
+        self.loading_label.setText("Loading...")
+        self.loading_label.setFont(QFont("Courier", 30, weight=QFont.Bold))
         self.loading_label.hide()
 
         self.graph_test = QLabel(self)
         self.graph_test.setGeometry(215, 10, 675, 580)
-        self.graph_test.setStyleSheet('background-color: #254059')
+        self.graph_test.setStyleSheet("background-color: #254059")
         self.graph_test.hide()
 
         self.statistic_predict = QLabel(self)
         self.statistic_predict.setGeometry(690, 10, 200, 400)
-        self.statistic_predict.setStyleSheet('background-color: #254059')
+        self.statistic_predict.setStyleSheet("background-color: #254059")
         self.statistic_predict.hide()
 
         self.tableWidget = QTableWidget()
@@ -354,9 +354,9 @@ class MainWindow(QMainWindow):
 
         self.dir_name = QFileDialog.getExistingDirectory(
             self,
-            'Select directory',
+            "Select directory",
             expanduser("~"),
-            QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog) + '/'
+            QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog) + "/"
 
         self.greetings.hide()
         self.button_test.hide()
@@ -368,8 +368,8 @@ class MainWindow(QMainWindow):
         self.button_refresh.setEnabled(False)
         self.repaint()
 
-        global_test(modelsDir, self.dir_name)
-        graph = QPixmap('test_stat.png')		
+        modelsTest(modelsDir, self.dir_name)
+        graph = QPixmap("test_stat.png")
         self.graph_test.setPixmap(graph.scaled(675, 580))
         self.graph_test.show()
         self.button_refresh.setEnabled(True)
@@ -379,9 +379,9 @@ class MainWindow(QMainWindow):
 
         self.dir_name = QFileDialog.getExistingDirectory(
             self,
-            'Select directory',
+            "Select directory",
             expanduser("~"),
-            QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog) + '/'
+            QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog) + "/"
 
         self.greetings.hide()
         self.button_test.hide()
@@ -393,7 +393,7 @@ class MainWindow(QMainWindow):
         self.button_refresh.setEnabled(False)
         self.repaint()
 
-        result = global_predict(modelsDir, self.dir_name)
+        result = modelsPrediction(modelsDir, self.dir_name)
 
         self.virus_label.hide()
 
@@ -401,10 +401,11 @@ class MainWindow(QMainWindow):
         self.tableWidget.setColumnCount(2)
         self.tableWidget.setRowCount(len(result))
         print(result)
-        result.sort()
-        for i in range(len(result)):
-            self.tableWidget.setItem(i, 0, QTableWidgetItem(result[i][0]))
-            self.tableWidget.setItem(i, 1, QTableWidgetItem((str(result[i][1])[:6]) + "%"))
+        j = 0
+        for i in result:
+            j += 1
+            self.tableWidget.setItem(j, 0, QTableWidgetItem(i))
+            self.tableWidget.setItem(j, 1, QTableWidgetItem((str(result[i])[:6]) + "%"))
 
         self.tableWidget.setMaximumSize(695, 600)
         self.tableWidget.setMinimumSize(695, 600)
--- pneumoniadetect.py
+++ pneumoniadetect.py
@@ -1,143 +1,112 @@
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from keras import models, backend
+from keras import models
 import os
 import matplotlib.pyplot as plt
+
+size = 199
+modelsDir = os.getcwd() + "/pneumonia/models/"
+
+# todo train DenseNet201, VGG16, InceptionResNetV2, Xception
 
 try:
     sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
     tf.compat.v1.keras.backend.set_session(sess)
-except:
-    print("Can't use the graphic card. Not found.")
-
-size = 199
-modelsDir = "pneumonia/models/"
+except Exception as e:
+    print("Can't use the graphics card. Not found. Exc: ", e)
 
 
-def processingImage(directory):
-    images, imageTitles, labels = [], [], []
-    for nextDirectory in os.listdir(directory):
-        if not nextDirectory.startswith("."):
-            print("next dir", nextDirectory)
-            if nextDirectory in "NORMAL":
-                label = 0
-            elif nextDirectory in "PNEUMONIA":
-                label = 1
-            else:
-                label = 2
+class PneumoniaDetect:
+    def __init__(self, modelsDirectory, inputDirectory):
+        self.modelsDirectory = modelsDirectory
+        self.inputDirectory = inputDirectory
+        self.images = []
+        self.imageTitles = []
+        self.labels = []
 
-            currentDirectory = directory + nextDirectory
-            dirs = directory
-            if currentDirectory.endswith(".jpg") or currentDirectory.endswith(".jpeg"):
-                print("Dir1", dirs)
-            else:
-                dirs = currentDirectory
-                print("Dir2", dirs)
-
-            for files in os.listdir(dirs):
-                if files.endswith(".jpg") or files.endswith(".jpeg"):
-                    imagePath = dirs + "/" + files
-                    imageTitles.append(files)
-
-                    img = Image.open(imagePath)
-                    img = img.resize((size, size)).convert("RGB")
-                    data = np.array(img.getdata())
-                    img = 2 * (data.reshape((img.size[0], img.size[1], 3)) / 255) - 1
-                    images.append(img)
-                    labels.append(label)
+    def processingImage(self, directory):
+        imagesTmp, labelsTmp = [], []
+        for nextDirectory in os.listdir(directory):
+            if not nextDirectory.startswith("."):
+                if nextDirectory in "NORMAL":
+                    label = 0
+                elif nextDirectory in "PNEUMONIA":
+                    label = 1
                 else:
-                    print("No such file extension")
-            if currentDirectory.endswith(".jpg") or currentDirectory.endswith(".jpeg"):
-                break
+                    label = 2
 
-    images = np.asarray(images)
-    labels = np.asarray(labels)
-    return imageTitles, images, labels
+                currentDirectory = directory + nextDirectory
+                dirs = directory
 
-
-def global_test(modelsDir, dir_test_image):
-    models_stat = []
-    num_mod = 0
-
-    _, images, labels = processingImage(dir_test_image)
-
-    average_accuracy = [0] * len(labels)
-
-    for file_model in os.listdir(modelsDir):
-        num_mod += 1
-
-        model_name = modelsDir + "/" + file_model
-        name = file_model[:-3]
-        model_stat = [name]
-
-        model = models.load_model(model_name)
-        predictions = model.predict(images, batch_size=1)
-        predictions = predictions.reshape(1, -1)[0]
-
-        accuracy_array = []
-
-        x = -1
-        for i in range(0, len(predictions), 2):
-            x += 1
-            if predictions[i] > predictions[i + 1]:
-                if labels[x] == 0:
-                    accuracy_array.append(predictions[i])
-                    average_accuracy[x] += predictions[i]
+                if currentDirectory.endswith(".jpg") or currentDirectory.endswith(".jpeg"):
+                    pass
                 else:
-                    accuracy_array.append(predictions[i + 1])
-                    average_accuracy[x] += predictions[i + 1]
+                    dirs = currentDirectory
 
-            if predictions[i] < predictions[i + 1]:
-                if labels[x] == 1:
-                    accuracy_array.append(predictions[i + 1])
-                    average_accuracy[x] += predictions[i + 1]
-                else:
-                    accuracy_array.append(predictions[i])
-                    average_accuracy[x] += predictions[i]
+                for files in os.listdir(dirs):
+                    if files.endswith(".jpg") or files.endswith(".jpeg"):
+                        imagePath = dirs + "/" + files
+                        self.imageTitles.append(files)
 
-        model_stat.append(np.mean(accuracy_array) * 100)
-        models_stat.append(model_stat)
+                        img = Image.open(imagePath)
+                        img = img.resize((size, size)).convert("RGB")
+                        data = np.array(img.getdata())
+                        img = 2 * (data.reshape((img.size[0], img.size[1], 3)) / 255) - 1
+                        imagesTmp.append(img)
+                        labelsTmp.append(label)
+                    else:
+                        print("No such file extension allowed")
+                if currentDirectory.endswith(".jpg") or currentDirectory.endswith(".jpeg"):
+                    break
 
-    models_stat.append(["Average", np.mean(average_accuracy) / num_mod * 100])
+        self.images = np.asarray(imagesTmp)
+        self.labels = np.asarray(labelsTmp)
 
-    x_data = []
-    y_data = []
-    for i in range(len(models_stat)):
-        x_data.append(models_stat[i][0])
-        y_data.append(models_stat[i][1])
-    _, ax = plt.subplots()
-    ax.bar(x_data, y_data, color='#539caf', align='center')
-    ax.set_ylabel("Accuracy")
-    ax.set_xlabel("Models")
-    plt.savefig("test_stat.png")
+    def modelsTest(self):
+        modelStat = {}
+        self.processingImage(self.inputDirectory)
+
+        for modelPath in os.listdir(self.modelsDirectory):
+            dirModel = self.modelsDirectory + modelPath
+            name = modelPath.split(".")[0]
+            model = models.load_model(dirModel)
+            predictions = model.predict(self.images)
+            accuracy = 0
+            for i in range(len(predictions)):
+                if self.labels[i] == np.argmax(predictions[i]):
+                    accuracy += 1
+            modelStat[name] = accuracy / len(predictions) * 100
+
+        # for key, value in modelStat.items():
+        #     print(f"Model is {key}\nAccuracy of predictions is: {value}%")
+
+        _, ax = plt.subplots()
+        ax.bar(modelStat.keys(), modelStat.values(), color="#539caf", align="center")
+        ax.set_ylabel("Accuracy")
+        ax.set_xlabel("Models")
+        plt.savefig("test_stat.png")
+
+    def modelsPrediction(self):
+        imagesStat = {}
+
+        self.processingImage(self.inputDirectory)
+
+        for modelPath in os.listdir(self.modelsDirectory):
+            dirModel = self.modelsDirectory + modelPath
+            model = models.load_model(dirModel)
+            predictions = model.predict(self.images, batch_size=1)
+
+            for i in range(len(predictions)):
+                if self.imageTitles[i] not in imagesStat:
+                    imagesStat[self.imageTitles[i]] = []
+                imagesStat[self.imageTitles[i]].append(predictions[i][1])
+
+        for i in imagesStat:
+            imagesStat[i] = np.mean(imagesStat[i]) * 100
+
+        return imagesStat
 
 
-def global_predict(modelsDir, dir_name):
-    images_stat = []
-    num_mod = 0
-
-    imageTitles, images, _ = processingImage(dir_name)
-
-    for file_model in os.listdir(modelsDir):
-        num_mod += 1
-        model_name = modelsDir + "/" + file_model
-
-        model = models.load_model(model_name)
-        predictions = model.predict(images, batch_size=1)
-        predictions = predictions.reshape(1, -1)[0]
-
-        accuracy_array = []
-
-        for i in range(0, len(predictions), 2):
-            accuracy_array.append(predictions[i + 1])
-        images_stat.append(accuracy_array)
-
-    result = []
-    for i in range(len(imageTitles)):
-        result.append([imageTitles[i], np.mean([images_stat[k][i] for k in range(len(images_stat))]) * 100])
-    return result
-
-
-print(global_predict("/home/push/Documents/GitHub/Python-Project-Pneumonia-Detect/pneumonia/models/",
-                     "/home/push/Documents/GitHub/Python-Project-Pneumonia-Detect/pneumonia/predict/"))
+# pd = PneumoniaDetect(os.getcwd()+"/pneumonia/models/", os.getcwd()+"/pneumonia/predict/")
+# print(pd.modelsPrediction())
